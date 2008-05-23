1adcfbc81b7f26ef627c35399f34888d476db7d5
puttiz lin <puttiz@gmail.com>
2008-05-23 11:24:35 +0800

    updated to edgerails

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,7 +3,13 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-  # See ActionController::RequestForgeryProtection for details
+ 
+ # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '87cb613f8da9348a68e93a388ad15a6f'
+
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password"). 
+  # filter_parameter_logging :password
 end
