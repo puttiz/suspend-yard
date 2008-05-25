73c4049ea814d5f0f3f050a8cc6b5e3fbcba7f5f
puttiz lin <puttiz@gmail.com>
2008-05-25 16:30:52 +0800

    changed users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,25 @@
 class UsersController < ApplicationController
 
-  make_resourceful do
-    actions :all
+  #make_resourceful do
+    #actions :all
 
-    #response_for :create do
-      ##redirect_to user_path(@user)
-      #redirect_to edit_user_path(@user)
-    #end
-  end
+    ##response_for :create do
+      ###redirect_to user_path(@user)
+      ##redirect_to edit_user_path(@user)
+    ##end
+  #end
 
-  def activate
-    #respond_to do |format|
-      #format.html do
-        #self.current_user = User.find_by_token(params[:key])
-        #if logged_in? && !current_user.activated?
-          #current_user.toggle! :activated
-          #flash[:notice] = "Signup complete!"[:signup_complete_message]
-        #end
-        #redirect_to CGI.unescape(params[:to] || home_path)
-      #end
-    #end
-  end
+  #def activate
+    ##respond_to do |format|
+      ##format.html do
+        ##self.current_user = User.find_by_token(params[:key])
+        ##if logged_in? && !current_user.activated?
+          ##current_user.toggle! :activated
+          ##flash[:notice] = "Signup complete!"[:signup_complete_message]
+        ##end
+        ##redirect_to CGI.unescape(params[:to] || home_path)
+      ##end
+    ##end
+  #end
 
 end
